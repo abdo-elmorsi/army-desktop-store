4704085d9ac0ad30ba25d9771058f4cc1507fa45
abdo-elmorsi <abdelrahmandiv@gmail.com>
2024-09-02 15:35:31 +0300

    lazy loading

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,19 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, lazy, Suspense } from 'react';
 import { HashRouter as Router, Route, Routes } from 'react-router-dom';
-import { Home, Products, Stores, Units, Settings, SignUp, Login, NotFound } from '@/pages';
 import { Layout } from '@/components';
-import ProductsForm from '@/pages/products/add-update';
-import StoreForm from '@/pages/stores/add-update';
-import UnitForm from './pages/units/add-update';
+
+// Lazy load the pages
+const Home = lazy(() => import('@/pages/Home'));
+const Products = lazy(() => import('@/pages/Products'));
+const Stores = lazy(() => import('@/pages/Stores'));
+const Units = lazy(() => import('@/pages/Units'));
+const Settings = lazy(() => import('@/pages/Settings'));
+const SignUp = lazy(() => import('@/pages/SignUp'));
+const Login = lazy(() => import('@/pages/Login'));
+const NotFound = lazy(() => import('@/pages/NotFound'));
+const ProductsForm = lazy(() => import('@/pages/products/add-update'));
+const StoreForm = lazy(() => import('@/pages/stores/add-update'));
+const UnitForm = lazy(() => import('@/pages/units/add-update'));
 
 function App() {
 
@@ -14,32 +23,33 @@ function App() {
 
     document.documentElement.classList.toggle('dark', savedDarkMode);
     document.documentElement.style.fontSize = `${savedFontSize.value}px`
-
   }, []);
+
   return (
     <Router>
-      <Routes>
-        <Route path="/" element={<Layout><Home /></Layout>} />
-        <Route path="/view" element={<Home view={true} />} />
+      <Suspense fallback={<h3 className='text-center mt-5'>تحميل...</h3>}>
+        <Routes>
+          <Route path="/" element={<Layout><Home /></Layout>} />
+          <Route path="/view" element={<Home view={true} />} />
 
-        <Route path="/products" element={<Layout><Products /></Layout>} />
-        <Route path="/products/add" element={<Layout><ProductsForm /></Layout>} />
-        <Route path="/products/edit/:id" element={<Layout><ProductsForm /></Layout>} />
+          <Route path="/products" element={<Layout><Products /></Layout>} />
+          <Route path="/products/add" element={<Layout><ProductsForm /></Layout>} />
+          <Route path="/products/edit/:id" element={<Layout><ProductsForm /></Layout>} />
 
-        <Route path="/stores" element={<Layout><Stores /></Layout>} />
-        <Route path="/stores/add" element={<Layout><StoreForm /></Layout>} />
-        <Route path="/stores/edit/:id" element={<Layout><StoreForm /></Layout>} />
+          <Route path="/stores" element={<Layout><Stores /></Layout>} />
+          <Route path="/stores/add" element={<Layout><StoreForm /></Layout>} />
+          <Route path="/stores/edit/:id" element={<Layout><StoreForm /></Layout>} />
 
-        <Route path="/units" element={<Layout><Units /></Layout>} />
-        <Route path="/units/add" element={<Layout><UnitForm /></Layout>} />
-        <Route path="/units/edit/:id" element={<Layout><UnitForm /></Layout>} />
+          <Route path="/units" element={<Layout><Units /></Layout>} />
+          <Route path="/units/add" element={<Layout><UnitForm /></Layout>} />
+          <Route path="/units/edit/:id" element={<Layout><UnitForm /></Layout>} />
 
-
-        <Route path="/settings" element={<Layout><Settings /></Layout>} />
-        <Route path="/login" element={<Login />} />
-        <Route path="/sign-up" element={<SignUp />} />
-        <Route path="*" element={<NotFound />} />
-      </Routes>
+          <Route path="/settings" element={<Layout><Settings /></Layout>} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/sign-up" element={<SignUp />} />
+          <Route path="*" element={<NotFound />} />
+        </Routes>
+      </Suspense>
     </Router>
   );
 }
